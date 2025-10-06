d04153929b68640a1d82d42154613989182a7fb6
DSofia-S <zerausannad@gmail.com>
2025-10-06 14:24:54 -0500

    ejercicio N2

--- src/Ejercicios/02-template-string.ts
+++ src/Ejercicios/02-template-string.ts
@@ -7,5 +7,5 @@ const semestre: number = 9;
 // usando el formato `${variable}` y se escriben con comillas invertidas (`).
 const mensaje: string = `Hola, mi nombre es ${nombre}, estudio ${carrera} y actualmente estoy en el semestre ${semestre}.`;
 
-// Mostramos el mensaje en la consola.
+// Mostramos el mensaje en la consola. hola
 console.log(mensaje);
