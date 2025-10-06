5c0e88b7d658e429489a8a761c9098835d266251
DSofia-S <zerausannad@gmail.com>
2025-10-06 14:15:38 -0500

    ejercicio N1

---
+++ src/Ejercicios/01-variables.ts
@@ -0,0 +1,12 @@
+// Declaración de una variable constante para el nombre del estudiante.
+const nombreEstudiante: string = "Danna";
+
+// Declaración de una variable con 'let' para la edad.gi
+let edad: number = 20;
+
+// Actualizamos el valor de la variable 'edad'.
+edad = 21;
+
+// Imprimimos ambos valores en la consola.
+console.log("Nombre del estudiante:", nombreEstudiante);
+console.log("Edad del estudiante:", edad);
