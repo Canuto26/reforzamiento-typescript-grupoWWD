42f629321e859f2509439544ff54832bc2c560ba
DSofia-S <zerausannad@gmail.com>
2025-10-05 22:10:54 -0500

    Se agrega el archivo 01-variables.ts

---
+++ src/Ejercicios/01-variables.ts
@@ -0,0 +1,13 @@
+// Declaración de una variable constante para el nombre del estudiante.
+const nombreEstudiante: string = "Danna";
+
+// Declaración de una variable con 'let' para la edad.
+let edad: number = 20;
+
+// Actualizar el valor de la variable 'edad'.
+edad = 21;
+
+// Imprimir ambos valores en la consola.
+console.log("Nombre del estudiante:", nombreEstudiante);
+console.log("Edad del estudiante:", edad);
+
