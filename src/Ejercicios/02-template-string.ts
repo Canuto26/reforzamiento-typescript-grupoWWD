8a9c85649124f4eea635944fe0dbff7a7d65886a
DSofia-S <105227758+DSofia-S@users.noreply.github.com>
2025-10-06 14:29:50 -0500

    Create 02-template-string.ts

---
+++ src/Ejercicios/02-template-string.ts
@@ -0,0 +1,11 @@
+// Declaracion de las variables necesarias.
+const nombre: string = "Danna";
+const carrera: string = "Ingeniería de Sistemas";
+const semestre: number = 9;
+
+// Usamos una plantilla de cadena (template string) para construir el mensaje.
+// usando el formato `${variable}` y se escriben con comillas invertidas (`).
+const mensaje: string = `Hola, mi nombre es ${nombre}, estudio ${carrera} y actualmente estoy en el semestre ${semestre}.`;
+
+// Mostramos el mensaje en la consola. hola
+console.log(mensaje);
